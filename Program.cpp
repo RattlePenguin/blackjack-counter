23f685aeb6049c1c9cc6732184c1cc311f6e5951
RattlePenguin <viggo.setiawan@gmail.com>
2026-02-01 22:54:26 +0800

    Drafted some program details, but no functional code yet.

---
+++ Program.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+/**
+ *  Program needs to initialize the deck of cards (shuffled) then play the game.
+ *  The program loop should be something like:
+ *  Dealer hand:   X 10
+ *  Your hand:     2 7
+ *  Player 2 hand: 5 2
+ *  ...
+ *  
+ *  h - Hit
+ *  s - Stand
+ *  d - Double down
+ *  t - split
+ *  y - surrender
+ *
+ */
+
+int main() {
+	std::cout << "How many players at the table?" << '\n';
+
+	bool gameActive = true;
+
+	while (gameActive) {
+		std::cout << "Dealer hand: " << '\n';
+		std::cout << "Your hand:   " << '\n';
+		
+		
+
+	}
+}
