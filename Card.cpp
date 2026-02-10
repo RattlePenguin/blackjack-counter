7387748b1800577783fcd0da0f6a28eeff09cb63
RattlePenguin <viggo.setiawan@gmail.com>
2026-02-10 23:58:49 +0800

    Added Card class with simple rank + suit member variables and implementations for getting value, count value and toString functions.

--- Card.cpp
+++ Card.cpp
@@ -1,4 +1,33 @@
-class Card {
+#include <string>
+#include "Enums.h"
 
-	Card(Suit suit, Rank rank) : suit(suit), rank(rank) {}
-}
+class Card {
+	Rank rank;
+	Suit suit;
+	bool faceDown;
+	
+	/**
+	 *  Returns the value of the card as per Blackjack rules.
+	 */
+	int getValue() const {
+		if (rank > Rank::TEN) return 10;
+		if (rank == Rank::ACE) return 11;
+		return static_cast<int>(rank);
+	}
+	
+	/**
+	 *  Returns the value contributing to the running count as per the Hi-Lo counting method.
+	 */
+	int getCountValue() const {
+		if (rank >= Rank::TWO && rank <= Rank::SIX) return 1;
+		if (rank >= Rank::TEN || rank == Rank::ACE) return -1;
+		return 0;
+	}
+
+	std::string toString() const {
+		if (faceDown) return "[XX]";
+		std::string rankChar[] { "", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+		return rankChar[static_cast<int>(rank)];
+	}
+
+};
