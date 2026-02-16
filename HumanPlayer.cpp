0696b2875a7e3eeae6e9b8dc3e1955530126fd68
RattlePenguin <viggo.setiawan@gmail.com>
2026-02-16 12:59:34 +1100

    Implemented makeDecision in HumanPlayer class.

--- HumanPlayer.cpp
+++ HumanPlayer.cpp
@@ -1,6 +1,44 @@
 #include "HumanPlayer.h"
+#include <iostream>
 
 HumanPlayer::HumanPlayer(std::string name) : Player(name) {}
 
-HumanPlayer::Action makeDecision(const Hand& currentHand, Card dealerUpCard, const BlackjackRules& rules);
+/**
+ *  Returns the HumanPlayer's Action depending on user input.
+ *  Verifies whether or not action is valid.
+ */
+Action HumanPlayer::makeDecision(const Hand& currentHand, Card dealerUpCard, const BlackjackRules& rules) {
+	bool isTwoCards { currentHand.isTwoCards() };
+	bool isPair { currentHand.isPair() };
+	
+	// Print Hands. This should be moved to more central, like game engine.
+	std::cout << "Dealer shows: " << dealerUpCard.toString() << '\n';
+	std::cout << "Your Hand: ";
+	currentHand.printHand();
+
+	std::cout << '\n';
+	
+	while (true) {
+		std::cout << "h: hit\n";
+		std::cout << "s: stand\n";
+		if (isTwoCards) std::cout << "d: double down\n";
+		if (isPair) std::cout << "t: split\n";
+		if (rules.surrenderAllowed && isTwoCards) std::cout << "y: surrender\n";
+
+		std::string input {};
+		std::cin >> input;
+		if (input == "h") {
+			return Action::HIT;
+		} else if (input == "s") {
+			return Action::STAND;
+		} else if (input == "d" && isTwoCards) {
+			return Action::DOUBLE;
+		} else if (input == "t" && isPair) {
+			return Action::SPLIT;
+		} else if (input == "y" && isTwoCards) {
+			return Action::SURRENDER;
+		}
+		std::cout << "Invalid Action!\n";
+	}
+}
 
