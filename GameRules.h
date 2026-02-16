1d3b7998112dcb0d8f3776cec1e58939f754dd50
RattlePenguin <viggo.setiawan@gmail.com>
2026-02-16 12:55:11 +1100

    Moved Test files to Test directory. Added HumanPlayerTest file that tests invalid but working inputs. Added compile_flags to resolve errors in includes. Removed redundant GameRules.h.

--- GameRules.h
+++
@@ -1,8 +0,0 @@
-struct GameRules {
-	int decks { 6 };
-	double penetration { 0.75 };
-	bool hitSoft17 { true };
-	bool doubleAfterSplit { true };
-	bool resplitAces { false };
-	bool surrenderAllowed { true };
-};
