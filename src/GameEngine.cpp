797181129a675b35bc435f36d162cc0d8452b4ef
RattlePenguin <viggo.setiawan@gmail.com>
2026-02-28 23:53:12 +1100

    Added some stuff

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -22,8 +22,6 @@ void GameEngine::addPlayer(Player* p) {
 }
 
 void GameEngine::playRound() {
-	// To play a round, you need to
-	// Check deck penetration, reshuffle if necessary
 	if (shoe.needsShuffle()) {
 		std::cout << "[Dealer] Shuffling the shoe...\n";
 		shoe.shuffle();
@@ -32,4 +30,22 @@ void GameEngine::playRound() {
 	// Ask player 1 for decision
 	// ... Clockwise order
 	//
+	//
+	// 2. Bets & Initial Deal
+        dealerHand = new Hand(0); // Dealer has no bet
+        
+        // Deal 2 cards to everyone
+        // Standard procedure: Card 1 to players, Card 1 to dealer, Card 2 to players, Card 2 to dealer.
+        for (int i = 0; i < 2; ++i) {
+            for (auto* p : players) {
+                if (i == 0) {
+                     // In a real app, ask for bet amount here.
+                     // For now, fixed bet of 10.
+                     p->startNewHand(10.0); 
+                }
+                // Deal to the LAST hand of the player (since they only have 1 right now)
+                p->hands.back().addCard(shoe.deal());
+            }
+            dealerHand->addCard(shoe.deal());
+        }
 }
