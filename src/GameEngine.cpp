9e3de711dbefc339a588e6d74c4b0757dcbbe4d3
RattlePenguin <viggo.setiawan@gmail.com>
2026-03-19 23:47:55 +1100

    Moving playRound functionality to helpers.

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -33,88 +33,15 @@ void GameEngine::playRound() {
 	}
 
 	startHands();
-
+	dealerPreCheck();
 	Card dealerUpCard = dealerHand->getCards().front(); // First card is the up card
-	
-	if (dealerUpCard.rank == Rank::ACE) {
-		std::cout << "[Dealer] Ace showing. Insurance? Y/N INCOMPLETE\n";
-	}
-
-	if (dealerHand->isBlackjack()) {
-		std::cout << "[Dealer] shows Blackjack.\n";
-		resolveRound();
-		return;
-	}
 
 	// Clockwise, ask players for decisions and finish their game before moving to next player.
 	for (auto* p : players) {
 		// Iterate over all hands, dynamic sizing to allow splitting
 		for (int i = 0; i < static_cast<int>(p->hands.size()); ++i) {
 			Hand& currentHand { p->hands[static_cast<size_t>(i)] };
-			while (!currentHand.isFinished()) {
-				printTurn(currentHand);
-
-				if (currentHand.isBusted()) {
-					std::cout << "---> BUSTED\n";
-					currentHand.finish();
-					break;
-				}
-
-				if (currentHand.isBlackjack()) {
-					std::cout << "---> BLACKJACK!\n";
-					currentHand.finish();
-					break;
-				}
-
-				if (currentHand.is21()) {
-					std::cout << "---> 21\n";
-					currentHand.finish();
-					break;
-				}
-
-				Action action { p->makeDecision(currentHand, rules) };
-
-				switch (action) {
-					case Action::HIT:
-						std::cout << "---> HIT\n";
-						currentHand.addCard(shoe.draw());
-						break;
-					case Action::STAND:
-						std::cout << "---> STAND\n";
-						currentHand.finish();
-						break;
-					case Action::DOUBLE:
-						std::cout << "---> DOUBLE DOWN\n";
-						currentHand.doubleDown();
-						currentHand.addCard(shoe.draw());
-						currentHand.finish();
-						break;
-					case Action::SPLIT: {
-						std::cout << "---> SPLIT\n";
-						Hand splitHand { p->startNewHand(currentHand.getBet()) };
-						Card splitCard { currentHand.pop_back() };
-						splitHand.addCard(splitCard);
-						currentHand.split();
-						splitHand.split();
-						
-						// Hit each hand
-						currentHand.addCard(shoe.draw());
-						splitHand.addCard(shoe.draw());
-						
-						// Split Aces get one card only.
-						if (splitCard.rank == Rank::ACE) {
-							currentHand.finish();
-							splitHand.finish();
-						}
-						break;
-					}
-					case Action::SURRENDER:
-						std::cout << "---> SURRENDER\n";
-						// TODO HANDLE BETS
-						currentHand.finish();
-						break;
-				}
-			}
+			doTurn(currentHand);
 		}
 	}
 
@@ -133,7 +60,6 @@ void GameEngine::startHands() {
 	for (int i = 0; i < 2; ++i) {
 		for (auto* p : players) {
 			if (i == 0) {
-				 // In a real app, ask for bet amount here.
 				 // For now, fixed bet of 10.
 				 p->startNewHand(10.0); 
 			}
@@ -152,13 +78,79 @@ void GameEngine::startHands() {
 
 }
 
+void GameEngine::dealerPreCheck() {
+	if (dealerHand->getCards().front().rank == Rank::ACE) {
+		std::cout << "[Dealer] Ace showing. Insurance? Y/N INCOMPLETE\n";
+	}
+	
+	// Peek
+	if (rules.lateSurrender && dealerHand->isBlackjack()) {
+		std::cout << "[Dealer] shows Blackjack.\n";
+		resolveRound();
+		return;
+	}
+}
+
+void doTurn(Hand& currentHand) {
+	while (!currentHand.isFinished()) {
+		printTurn(currentHand);
+		
+		if (currentHand.getRealValue() >= 21) {
+			std::cout << "---> BUSTED\n";
+			currentHand.finish();
+			break;
+		}
+
+		Action action { p->makeDecision(currentHand, rules) };
+
+		switch (action) {
+			case Action::HIT:
+				std::cout << "---> HIT\n";
+				currentHand.addCard(shoe.draw());
+				break;
+			case Action::STAND:
+				std::cout << "---> STAND\n";
+				currentHand.finish();
+				break;
+			case Action::DOUBLE:
+				std::cout << "---> DOUBLE DOWN\n";
+				currentHand.doubleDown();
+				currentHand.addCard(shoe.draw());
+				currentHand.finish();
+				break;
+			case Action::SPLIT: {
+				std::cout << "---> SPLIT\n";
+				Hand splitHand { p->startNewHand(currentHand.getBet()) };
+				Card splitCard { currentHand.pop_back() };
+				splitHand.addCard(splitCard);
+				currentHand.split();
+				splitHand.split();
+				
+				// Hit each hand
+				currentHand.addCard(shoe.draw());
+				splitHand.addCard(shoe.draw());
+				
+				// Split Aces get one card only.
+				if (splitCard.rank == Rank::ACE) {
+					currentHand.finish();
+					splitHand.finish();
+				}
+				break;
+			}
+			case Action::SURRENDER:
+				std::cout << "---> SURRENDER\n";
+				currentHand.surrender();
+				currentHand.finish();
+				break;
+		}
+	}
+}
+
 /**
  *  Hands have finished, handle payouts.
  *  Players modify their own bankrolls for profit/loss.
 */
 void GameEngine::resolveRound() {
-	bool dBlackjack { dealerHand->isBlackjack() };
-
 	for (auto* p : players) {
 		std::cout << p->getName() << " results\n";
 		for (const Hand& h : p->hands) {
@@ -166,36 +158,34 @@ void GameEngine::resolveRound() {
 			h.printHand();
 
 			if (h.isBusted()) {
-				std::cout << "Lost " << h.getBet();
+				std::cout << "Lost " << h.getBet() << '\n';
 				p->lose(h.getBet());
 				continue;
 			}
-
 			if (dealerHand->isBusted()) {
-				std::cout << "Win " << h.getBet();
+				std::cout << "Win " << h.getBet() << '\n';
 				p->win(h.getBet());
 				continue;
 			}
 			
 			// Natural blackjacks win
-			if (dBlackjack && !h.isBlackjack()) {
-				std::cout << "Lost " << h.getBet();
+			if (dealerHand->isBlackjack() && !h.isBlackjack()) {
+				std::cout << "Lost " << h.getBet() << '\n';
 				p->lose(h.getBet());
 				continue;
-			} else if (!dBlackjack && h.isBlackjack()) {
+			} else if (!dealerHand->isBlackjack() && h.isBlackjack()) {
 				p->win(h.getBet());
-				std::cout << "Won " << h.getBet();
+				std::cout << "Won " << h.getBet() << '\n';
 				continue;
 			}
 
 			if (dealerHand->getRealValue() < h.getRealValue()) {
 				p->win(h.getBet());
-				std::cout << "Won " << h.getBet();
+				std::cout << "Won " << h.getBet() << '\n';
 			} else if (dealerHand->getRealValue() > h.getRealValue()) {
 				p->lose(h.getBet());
-				std::cout << "Lost " << h.getBet();
+				std::cout << "Lost " << h.getBet() << '\n';
 			}
-			// push
 		}
 	}
 }
