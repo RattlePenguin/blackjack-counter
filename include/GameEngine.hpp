af9b97c95e1d8e093ebd70b6fd7bb547a041a669
RattlePenguin <viggo.setiawan@gmail.com>
2026-03-19 23:48:28 +1100

    Added member functions and some comments.

--- include/GameEngine.hpp
+++ include/GameEngine.hpp
@@ -12,9 +12,17 @@ class GameEngine {
 public:
 	GameEngine(int numDecks, double penetration);
 	~GameEngine();
-
+	
+	/**
+	 *  Adds a player to the game.
+	 */
 	void addPlayer(Player* player);
+
+	/**
+	 *  Starts a game of Blackjack.
+	 */
 	void playRound();
+
 private:
 	Shoe shoe;
 	BlackjackRules rules {};
@@ -23,6 +31,7 @@ private:
 
 	bool dealerShouldHit();
 	void startHands();
+	void dealerPreCheck();
 	void resolveRound();
 	void printTurn(Hand& hand);
 	void dealerTurn();
