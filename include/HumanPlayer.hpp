24f6bb9a1b1696b472e9ba766d507a319936c7af
RattlePenguin <viggo.setiawan@gmail.com>
2026-04-15 03:15:56 +1000

    Added func decl for player getting hints and strategy summary.

--- include/HumanPlayer.hpp
+++ include/HumanPlayer.hpp
@@ -6,11 +6,22 @@
 #include "Hand.hpp"
 #include "Player.hpp"
 #include "BlackjackRules.hpp"
+#include "StrategyAdvisor.hpp"
+#include <vector>
 
 class HumanPlayer : public Player  {
 public:
 	HumanPlayer(std::string name);
-	Action makeDecision(const Hand& currentHand, const BlackjackRules& rules);
+	Action makeDecision(const Hand& currentHand, const Card& dealerUpCard, const BlackjackRules& rules) override;
+	
+	void setHints(bool show);
+	bool getHints() const;
+	void printStrategySummary() const;
+	void clearStrategyRecords();
+
+private:
+	bool showHints { false };
+	std::vector<StrategyRecord> strategyRecords {};
 };
 
 #endif // !HUMANPLAYER_H
