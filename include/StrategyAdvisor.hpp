cd38bddd39bb64682ec1b49697f901b268efa128
RattlePenguin <viggo.setiawan@gmail.com>
2026-04-15 03:07:50 +1000

    Added header comments.

--- include/StrategyAdvisor.hpp
+++ include/StrategyAdvisor.hpp
@@ -18,12 +18,32 @@ struct StrategyRecord {
 
 class StrategyAdvisor {
 public:
+	/**
+	 *  Returns the recommended action given the current hand and rules.
+	 */
     static Action getRecommendedAction(const Hand& playerHand, const Card& dealerUpCard, const BlackjackRules& rules);
+
+	/**
+	 *  Returns the given action as a string.
+	 */
     static std::string actionToString(Action action);
 
 private:
+	/**
+	 *  Returns the recommended action given the player's current hand as a hard value.
+	 *  Compares against dealer value.
+	 *  Recommended action can vary depending on rules.
+	 */
     static Action getHardAction(int value, int dealerValue, const BlackjackRules& rules, bool canDouble, bool canSurrender);
+	
+	/**
+	 *  Returns the recommended action given the player's current hand as a soft value.
+	 */
     static Action getSoftAction(int value, int dealerValue, const BlackjackRules& rules, bool canDouble);
+
+	/**
+	 *  Returns the recommended action given the player's current hand as a pair.
+	 */
     static Action getPairAction(Rank pairRank, int dealerValue, const BlackjackRules& rules);
 };
 
